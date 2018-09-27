b42ce9f6625d97581f1da96d5b262505d05b50af
venkatalolla <suryasaicharan93@gmail.com>
2018-09-26 20:46:01 -0400

    Initial test

---
+++ src/test/java/org/jboss/as/quickstarts/tasksrs/service/AppTest.java
@@ -0,0 +1,53 @@
+package org.jboss.as.quickstarts.tasksrs.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	private static final String CHROME_PATH = "/usr/bin/chromedriver";
+	private static ChromeDriverService service;
+	private WebDriver driver;
+	String appURL = "http://tasks-tasks-dev.oseapps.levvel-labs.io/";
+
+	@BeforeClass
+	public static void createAndStartService() throws IOException
+	{
+		service = new ChromeDriverService.Builder().usingDriverExecutable(new File(CHROME_PATH)).usingAnyFreePort().build();
+		service.start();
+	}
+
+	@BeforeClass
+	public void testSetUp()
+	{
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--no-sandbox");
+		chromeOptions.addArguments("--disable-dev-shm-usage");
+		driver = new ChromeDriver(service, chromeOptions);
+	}
+
+	@Test
+	public void verifyOpenShiftTasksPageTittle()
+	{
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "OpenShift Demo Tasks");
+	}
+
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
