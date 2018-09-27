e94c9baa7c03fb79dd068734ea2bac25894460e2
Venkata Lolla <suryasaicharan93@gmail.com>
2018-09-26 23:19:27 -0400

    Update TaskResourceTest.java

--- src/test/java/org/jboss/as/quickstarts/tasksrs/service/TaskResourceTest.java
+++ src/test/java/org/jboss/as/quickstarts/tasksrs/service/TaskResourceTest.java
@@ -1,57 +1,118 @@
 package org.jboss.as.quickstarts.tasksrs.service;
 
-import java.io.File;
-import java.io.IOException;
+import static java.util.Arrays.asList;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.Principal;
+
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.jboss.as.quickstarts.tasksrs.category.UnitTest;
+import org.jboss.as.quickstarts.tasksrs.model.Task;
+import org.jboss.as.quickstarts.tasksrs.model.TaskDao;
+import org.jboss.as.quickstarts.tasksrs.model.User;
+import org.jboss.as.quickstarts.tasksrs.model.UserDao;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+@RunWith(MockitoJUnitRunner.class)
 @Category(UnitTest.class)
 public class TaskResourceTest {
+	@InjectMocks
+	private TaskResource taskResource;
+	
+	@Mock
+    private UserDao userDao;
 
-	private static final String CHROME_PATH = "/usr/bin/chromedriver";
-	//private static final String CHROME_PATH = "/Users/suryalolla/Documents/GitHub/openshift-tasks/chromedriver";
-	private static ChromeDriverService service;
-	private WebDriver driver;
-	String appURL = "http://tasks-tasks-dev.oseapps.levvel-labs.io/";
-
-	@BeforeClass
-	public static void createAndStartService() throws IOException
-	{
-		service = new ChromeDriverService.Builder().usingDriverExecutable(new File(CHROME_PATH)).usingAnyFreePort().build();
-		service.start();
+    @Mock
+    private TaskDao taskDao;
+    
+    @Mock
+    private SecurityContext securityContext;
+    
+    @Mock
+    private UriInfo uriInfo;
+	
+    @Before
+    public void setupMocks() throws URISyntaxException {
+    	Principal principal = mock(Principal.class);
+    	when(securityContext.getUserPrincipal()).thenReturn(principal);
+    	when(principal.getName()).thenReturn("sally");
+    	
+    	
+    	// mock response
+    	URI uri = new URI("/response");
+    	UriBuilder uriBuilder = mock(UriBuilder.class);
+    	when(uriInfo.getAbsolutePath()).thenReturn(uri);
+    	when(uriInfo.getAbsolutePathBuilder()).thenReturn(uriBuilder);
+    	when(uriBuilder.replacePath(anyString())).thenReturn(uriBuilder);
+    	when(uriBuilder.build()).thenReturn(uri);
+    	
+    	// set id on new tasks
+    	Mockito.doAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				Task task = (Task)invocation.getArgument(1);
+				task.setId(System.currentTimeMillis());
+				return null;
+			}
+		}).when(taskDao).createTask(any(User.class), any(Task.class));
+    }
+    
+    
+	@Test
+	public void createTaskWithNewUser() {
+		taskResource.createTask(uriInfo, securityContext, "run a demo!");
+		
+		verify(userDao).createUser(any(User.class));
+		verify(taskDao).createTask(any(User.class), any(Task.class));
 	}
-
-	@BeforeClass
-	public void testSetUp()
-	{
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--no-sandbox");
-		chromeOptions.addArguments("--disable-dev-shm-usage");
-		driver = new ChromeDriver(service, chromeOptions);
-	}
+	
 
 	@Test
-	public void verifyOpenShiftTasksPageTittle()
-	{
-		driver.navigate().to(appURL);
-		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "OpenShift Demo Tasks");
+	public void getTaskByIdWithNonExistingUser() {
+		when(taskDao.getAll(any(User.class))).thenReturn(asList(newTask(123L)));
+		
+		taskResource.getTaskById(securityContext, 123L);
+		
+		verify(userDao).getForUsername("sally");
+		verify(userDao).createUser(any(User.class));
+		verify(taskDao).getAll(any(User.class));
 	}
 
-	@AfterClass
-	public void tearDown()
-	{
-		driver.quit();
-	}
 
+	@Test
+	public void deleteTaskById() {
+		when(taskDao.getAll(any(User.class))).thenReturn(asList(newTask(123L)));
+		
+		taskResource.deleteTaskById(securityContext, 123L);
+		
+		verify(userDao).getForUsername("sally");
+		verify(userDao).createUser(any(User.class));
+		verify(taskDao).getAll(any(User.class));
+		verify(taskDao).deleteTask(any(Task.class));
+
+	}
+	
+	private Task newTask(Long id) {
+		Task task = new Task("Task 1");
+		task.setId(id);
+		return task;
+	}
 }
